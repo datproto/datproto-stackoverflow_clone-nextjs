e32e81fd4b603b1f49d3b2aed63992ce9f1de2ba
Dat Proto <contact@datproto.com>
2023-09-29 01:04:31 +0700

    Finish Navbar

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -4,20 +4,20 @@ import Image from 'next/image'
 import {SignedIn, UserButton} from '@clerk/nextjs'
 import Theme from '@/components/shared/navbar/Theme'
 import MobileNav from '@/components/shared/navbar/MobileNav'
+import GlobalSearch from '@/components/shared/search/GlobalSearch'
 
 const Navbar = () => {
   return (
     <nav
       className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-      <Link href="/" className="flex items-center sm:px-12">
-        <Image src="/assets/images/site-logo.svg" height={23} width={23} alt="DevFlow"
-               className="flex items-center gap-1"/>
+      <Link href="/" className="flex items-center gap-1">
+        <Image src="/assets/images/site-logo.svg" height={23} width={23} alt="DevFlow"/>
         <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-500 max-sm:hidden">Dev <span
           className="text-primary-500">Overflow</span>
         </p>
       </Link>
 
-      Global Search
+      <GlobalSearch/>
 
       <div className="flex-between gap-5">
         <Theme/>
