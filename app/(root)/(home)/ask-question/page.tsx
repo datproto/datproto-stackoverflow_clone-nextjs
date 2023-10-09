e9cd7bb7076f209fcd1c4fda09a96c4fdb4eee45
Dat Proto <contact@datproto.com>
2023-10-10 00:51:47 +0700

    ADD: Form Shadcn component. TODO: Add WYSIWYG editor

--- app/(root)/(home)/ask-question/page.tsx
+++ app/(root)/(home)/ask-question/page.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Question from '@/components/forms/Question'
 
 const Page = () => {
   return (
     <div>
-      Ask Question
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question/>
+      </div>
     </div>
   )
 }
