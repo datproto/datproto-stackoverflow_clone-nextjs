a04450ba31ea4b2e2fe84e1b0f5e03a7032b8a35
Dat Proto <contact@datproto.com>
2023-10-11 13:22:47 +0700

    ADD: Tag Input feature

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, {useRef} from 'react'
 
 import {zodResolver} from '@hookform/resolvers/zod'
 import * as z from 'zod'
@@ -10,8 +10,13 @@ import {Input} from '@/components/ui/input'
 import {useForm} from 'react-hook-form'
 import {Button} from '@/components/ui/button'
 import {questionSchema} from '@/lib/validations'
+import {Editor} from '@tinymce/tinymce-react'
+import {Badge} from '@/components/ui/badge'
+import Image from 'next/image'
 
 const Question = () => {
+  const editorRef = useRef(null)
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof questionSchema>>({
     resolver: zodResolver(questionSchema),
@@ -29,6 +34,42 @@ const Question = () => {
     console.log(values)
   }
 
+  const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, field: any) => {
+    if (e.key === 'Enter' && field.name === 'tags') {
+      e.preventDefault()
+
+      const tagInput = e.target as HTMLInputElement
+      const tagValue = tagInput.value.trim()
+
+      if (tagValue !== '') {
+        if (tagValue.length > 15) {
+          return form.setError('tags', {
+            type: 'required',
+            message: 'Tag must be less than 15 characters.'
+          })
+        }
+
+        if (!field.value.includes(tagValue as never)) {
+          form.setValue('tags', [...field.value, tagValue])
+          console.log(form)
+
+          tagInput.value = ''
+
+          form.clearErrors('tags')
+        } else {
+          form.trigger()
+        }
+      }
+    }
+  }
+
+  const handleTagRemove = (tag: string, field: any) => {
+    const newTags = field.value.filter((t: string) => t !== tag)
+
+    form.setValue('tags', newTags)
+  }
+
+  // @ts-ignore
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full flex-col gap-10">
@@ -63,7 +104,41 @@ const Question = () => {
               <FormLabel className="paragraph-semibold text-dark400_light800">Detailed explanation of your problem <span
                 className="text-primary-500">*</span></FormLabel>
               <FormControl className="mt-3.5">
-                {/* TODO: Add an Editor component */}
+                <Editor
+                  apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_KEY}
+                  onInit={
+                    (evt, editor) => {
+                      // @ts-ignore
+                      editorRef.current = editor
+                    }}
+                  initialValue="<p>This is the initial content of the editor.</p>"
+                  init={{
+                    height: 350,
+                    menubar: false,
+                    plugins: [
+                      'advlist',
+                      'autolink',
+                      'codesample',
+                      'lists',
+                      'link',
+                      'image',
+                      'charmap',
+                      'preview',
+                      'anchor',
+                      'searchreplace',
+                      'visualblocks',
+                      'codesamoke',
+                      'fullscreen',
+                      'insertdatetime',
+                      'media',
+                      'table'
+                    ],
+                    toolbar: 'undo redo | ' +
+                      'codesample | bold italic forecolor | alignleft aligncenter |' +
+                      'alignright alignjustify | bullist numlist ',
+                    content_style: 'body { font-family:Inter; font-size:16px }'
+                  }}
+                />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Introduce the problem and expand on what you put in the title. Minimum 20 characters.
@@ -83,11 +158,28 @@ const Question = () => {
               <FormLabel className="paragraph-semibold text-dark400_light800">Tags <span
                 className="text-primary-500">*</span></FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
-                  placeholder="Add tags ..."
-                  {...field}
-                />
+                <>
+                  <Input
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                    placeholder="Add tags ..."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag: any) => (
+                        <Badge key={tag}
+                               className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                               onClick={() => handleTagRemove(tag, field)}
+                        >
+                          {tag}
+                          <Image src="/assets/icons/close.svg" alt="close-icon" width={12} height={12}
+                                 className="cursor-pointer object-contain invert-0 dark:invert"/>
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
                 Add up to 3 tags describe what your question is about. You need to press enter to add a tag.
