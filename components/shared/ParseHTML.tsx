9d625ae13a2710d75fcf66da11f333405a317445
Dat Proto <contact@datproto.com>
2023-10-15 20:46:25 +0700

    INITIAL: Setup and layout

---
+++ components/shared/ParseHTML.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {IQuestion} from '@/lib/models/question.model'
+
+interface IParseHTML {
+  data: IQuestion
+}
+
+const ParseHtml = ({data}: IParseHTML) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ParseHtml
