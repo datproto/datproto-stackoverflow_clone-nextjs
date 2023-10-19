5e26b7cabbd4961efed20dff8ce1831ccd63e7b3
Dat Proto <contact@datproto.com>
2023-10-20 01:03:12 +0700

    FIX: Remove unnecessary vars

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -47,7 +47,7 @@ export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
   try {
     connectToDatabase()
 
-    const {tagId, page = 1, pageSize = 10, searchQuery} = params
+    const {tagId , searchQuery} = params
 
     const tagFilter: FilterQuery<ITag> = {_id: tagId}
     const tag = await Tag.findOne(tagFilter).populate(
