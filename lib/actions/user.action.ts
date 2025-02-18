c43d2e628ce374c50c511e7ed07061619fc78782
Dat Proto <contact@datproto.com>
2025-02-19 01:40:09 +0700

    fix: user.action errors

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import {connectToDatabase} from '@/lib/mongoose'
+import { connectToDatabase } from '@/lib/mongoose'
 import User from '@/lib/models/user.model'
 import {
   CreateUserParams,
@@ -9,7 +9,7 @@ import {
   GetUserByIdParams,
   UpdateUserParams
 } from '@/lib/shared.types'
-import {revalidatePath} from 'next/cache'
+import { revalidatePath } from 'next/cache'
 import Question from '@/lib/models/question.model'
 import Answer from '@/lib/models/answer.model'
 
@@ -17,11 +17,11 @@ export async function getUserById(params: GetUserByIdParams) {
   try {
     await connectToDatabase()
 
-    const {userId} = params
+    const { userId } = params
 
-    const user = await User.findOne({clerkId: userId})
+    const user = await User.findOne({ clerkId: userId })
 
-    if(!user) {
+    if (!user) {
       throw new Error('User not found')
     }
 
@@ -49,9 +49,9 @@ export async function updateUser(params: UpdateUserParams) {
   try {
     await connectToDatabase()
 
-    const {clerkId, updateData, path} = params
+    const { clerkId, updateData, path } = params
 
-    await User.findOneAndUpdate({clerkId}, updateData, {
+    await User.findOneAndUpdate({ clerkId }, updateData, {
       new: true
     })
 
@@ -66,20 +66,20 @@ export async function deleteUser(params: DeleteUserParams) {
   try {
     await connectToDatabase()
 
-    const {clerkId} = params
+    const { clerkId } = params
 
-    const user = await User.findOneAndDelete({clerkId})
+    const user = await User.findOneAndDelete({ clerkId })
 
-    if (!user) {
-      throw new Error('User no found')
+    if (!user.value) {
+      throw new Error('User not found')
     }
 
-    // const userQuestionIds = await Question.find({author: user._id})
+    // const userQuestionIds = await Question.find({author: user.value._id})
     //   .distinct('_id')
 
-    await Question.deleteMany({author: user._id})
+    await Question.deleteMany({ author: user.value._id })
 
-    const deletedUser = await User.findByIdAndDelete(user._id)
+    const deletedUser = await User.findByIdAndDelete(user.value._id)
 
     return deletedUser
   } catch (e) {
@@ -95,10 +95,10 @@ export async function getAllUsers(params: GetAllUsersParams) {
     // const {page = 1, pageSize = 20, filter, searchQuery} = params
 
     const users = await User.find({})
-      .sort({createdAt: -1})
+      .sort({ createdAt: -1 })
 
 
-    return {users}
+    return { users }
   } catch (e) {
     console.log(e)
     throw e
@@ -109,10 +109,10 @@ export async function getUserInfo(params: GetUserByIdParams) {
   try {
     await connectToDatabase()
 
-    const user = await getUserById({userId: params.userId})
+    const user = await getUserById({ userId: params.userId })
 
-    const totalQuestions = await Question.countDocuments({authors: user._id})
-    const totalAnswers = await Answer.countDocuments({authors: user._id})
+    const totalQuestions = await Question.countDocuments({ authors: user._id })
+    const totalAnswers = await Answer.countDocuments({ authors: user._id })
 
     return {
       user,
